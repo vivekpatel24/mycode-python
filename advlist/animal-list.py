ae2d06d2212e053ea58210849ee125064bdaae17
Vivek <vivekpatel24@users.noreply.github.com>
2022-05-25 13:29:42 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called list1
+    list1 = ["cow", "rat", "dog", "ant", "bee"]
+    
+    # display list1
+    print(list1)
+    
+main ()
+   
